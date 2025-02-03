05ba9d3e66b93ccec2ccce3aedc0dc2146000080
Ddtester5 <dd5892631@gmail.com>
2025-02-03 17:09:13 +0000

    and standart scripts and express

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,1 +1,12 @@
-console.log("hi");
+import express from "express";
+import { Request, Response } from "express";
+
+const app = express();
+
+app.get("/qwe", (req: Request, res: Response) => {
+  res.send("qwe");
+});
+
+app.listen("3000", () => {
+  console.log("Serever start at 3000 port");
+});
